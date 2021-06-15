f2cd5e42d36ecc027dba668894518a3e4c667938
Ian Macalinao <ian@saber.so>
2021-06-15 03:25:22 -0500

    Update token math library (#18)
    * Update token math library
    
    * Make spl-token library much smaller; re-export relevant types
    
    * v0.1.1-beta.4

--- packages/spl-token/src/index.ts
+++ packages/spl-token/src/index.ts
@@ -1,5 +1,23 @@
-export * from "./constants";
 export * from "./price";
 export * from "./token";
 export * from "./tokenAmount";
-export * from "./utils";
+
+// re-export token-math types
+// so consumers don't need to use them
+export {
+  BigintIsh,
+  Fraction,
+  IFormatUint,
+  makeDecimalMultiplier,
+  MAX_U64,
+  MAX_U256,
+  NumberFormat,
+  ONE,
+  parseBigintIsh,
+  Percent,
+  Rounding,
+  TEN,
+  validateU64,
+  validateU256,
+  ZERO,
+} from "@ubeswap/token-math";
--- packages/spl-token/src/tokenAmount.ts
+++ packages/spl-token/src/tokenAmount.ts
@@ -1,29 +1,12 @@
 import { u64 } from "@solana/spl-token";
 import {
-  Big,
-  Fraction,
-  NumberFormat,
+  BigintIsh,
   Percent,
-  Rounding,
+  TokenAmount as UTokenAmount,
+  validateU64,
 } from "@ubeswap/token-math";
-import Decimal from "decimal.js";
-import JSBI from "jsbi";
-import invariant from "tiny-invariant";
 
-import { MAX_U64, ZERO } from "./constants";
-import { Token, tokensEqual } from "./token";
-import { BigintIsh, makeDecimalMultiplier, parseBigintIsh } from "./utils";
-
-export function validateU64(value: JSBI): void {
-  invariant(
-    JSBI.greaterThanOrEqual(value, ZERO),
-    `${value.toString()} must be greater than zero`
-  );
-  invariant(
-    JSBI.lessThanOrEqual(value, MAX_U64),
-    `${value.toString()} overflows u64`
-  );
-}
+import { Token } from "./token";
 
 export interface IFormatUint {
   /**
@@ -36,28 +19,11 @@ export interface IFormatUint {
   locale?: string;
 }
 
-const stripTrailingZeroes = (num: string): string => {
-  const [head, tail, ...rest] = num.split(".");
-  if (rest.length > 0 || !head) {
-    console.warn(`Invalid number passed to stripTrailingZeroes: ${num}`);
-    return num;
-  }
-  if (!tail) {
-    return num;
-  }
-  return `${head}.${tail.replace(/\.0+$/, "")}`;
-};
-
-export class TokenAmount extends Fraction {
-  public readonly token: Token;
-
+export class TokenAmount extends UTokenAmount<Token> {
   // amount _must_ be raw, i.e. in the native representation
   public constructor(token: Token, amount: BigintIsh) {
-    const parsedAmount = parseBigintIsh(amount);
-    validateU64(parsedAmount);
-
-    super(parsedAmount, makeDecimalMultiplier(token.decimals));
-    this.token = token;
+    super(token, amount);
+    validateU64(this.raw);
   }
 
   /**
@@ -66,77 +32,26 @@ export class TokenAmount extends Fraction {
    * @param uiAmount
    * @returns
    */
-  public static parse(token: Token, uiAmount: string): TokenAmount {
-    return new TokenAmount(
-      token,
-      JSBI.BigInt(
-        new Decimal(uiAmount)
-          .times(new Decimal(10).pow(token.decimals))
-          .floor()
-          .toString()
-      )
-    );
+  public static parseAmount(token: Token, uiAmount: string): TokenAmount {
+    const prev = UTokenAmount.parse(token, uiAmount);
+    return new TokenAmount(token, prev.raw);
   }
 
-  public get raw(): JSBI {
-    return this.numerator;
+  add(other: TokenAmount): TokenAmount {
+    const result = super.add(other);
+    return new TokenAmount(this.token, result.raw);
   }
-
-  public toSignificant(
-    significantDigits = 6,
-    format?: NumberFormat,
-    rounding: Rounding = Rounding.ROUND_DOWN
-  ): string {
-    return super.toSignificant(significantDigits, format, rounding);
+  subtract(other: TokenAmount): TokenAmount {
+    const result = super.subtract(other);
+    return new TokenAmount(this.token, result.raw);
   }
-
-  public toFixed(
-    decimalPlaces: number = this.token.decimals,
-    format?: NumberFormat,
-    rounding: Rounding = Rounding.ROUND_DOWN
-  ): string {
-    invariant(decimalPlaces <= this.token.decimals, "DECIMALS");
-    return super.toFixed(decimalPlaces, format, rounding);
+  multiplyBy(percent: Percent): TokenAmount {
+    const result = super.multiplyBy(percent);
+    return new TokenAmount(this.token, result.raw);
   }
-
-  public toExact(format: NumberFormat = { groupSeparator: "" }): string {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    Big.DP = this.token.decimals;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-    return new Big(this.numerator)
-      .div(this.denominator.toString())
-      .toFormat(format);
-  }
-
-  public add(other: TokenAmount): TokenAmount {
-    invariant(tokensEqual(this.token, other.token), "TOKEN");
-    return new TokenAmount(this.token, JSBI.add(this.raw, other.raw));
-  }
-
-  public subtract(other: TokenAmount): TokenAmount {
-    invariant(tokensEqual(this.token, other.token), "TOKEN");
-    return new TokenAmount(this.token, JSBI.subtract(this.raw, other.raw));
-  }
-
-  /**
-   * Gets this TokenAmount as a percentage of the other TokenAmount.
-   * @param other
-   * @returns
-   */
-  public divideByAmount(other: TokenAmount): Percent {
-    invariant(tokensEqual(this.token, other.token), "TOKEN");
-    const frac = this.divide(other);
-    return new Percent(frac.numerator, frac.denominator);
-  }
-
-  /**
-   * Gets this TokenAmount as a percentage of the other TokenAmount.
-   * @param other
-   * @returns
-   */
-  public divideBy(other: Fraction): Percent {
-    const frac = this.divide(other);
-    return new Percent(frac.numerator, frac.denominator);
+  reduceBy(percent: Percent): TokenAmount {
+    const result = super.reduceBy(percent);
+    return new TokenAmount(this.token, result.raw);
   }
 
   /**
@@ -145,43 +60,5 @@ export class TokenAmount extends Fraction {
    */
   public toU64(): u64 {
     return new u64(this.raw.toString());
-  }
-
-  /**
-   * Multiplies this token amount by a percent.
-   * WARNING: this loses precision
-   * @param percent
-   * @returns
-   */
-  public multiplyBy(percent: Percent): TokenAmount {
-    return new TokenAmount(
-      this.token,
-      percent.asFraction.multiply(this.raw).toFixed(0)
-    );
-  }
-
-  /**
-   * Reduces this token amount by a percent.
-   * WARNING: this loses precision
-   * @param percent
-   * @returns
-   */
-  public reduceBy(percent: Percent): TokenAmount {
-    return this.multiplyBy(new Percent(1, 1).subtract(percent));
-  }
-
-  /**
-   * Formats this number using Intl.NumberFormatOptions
-   * @param param0
-   * @returns
-   */
-  public format({ numberFormatOptions, locale }: IFormatUint = {}): string {
-    const asExactString = this.toFixed(this.token.decimals);
-    const asNumber = parseFloat(asExactString);
-    return `${
-      numberFormatOptions !== undefined
-        ? asNumber.toLocaleString(locale, numberFormatOptions)
-        : stripTrailingZeroes(asExactString)
-    }`;
   }
 }
