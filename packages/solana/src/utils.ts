e7894f93e2357b8c20a00f50a654a1d7b66abda6
Ian Macalinao <ian@saber.so>
2021-07-20 20:07:58 -0400

    PublicKey checker (#107)
    * PK checker
    
    * arrays are not PublicKeys

---
+++ packages/solana/src/utils.ts
@@ -0,0 +1,23 @@
+import { PublicKey, PublicKeyData } from "@solana/web3.js";
+
+/**
+ * Returns true if the given value is a {@link PublicKey}.
+ * @param pk
+ * @returns
+ */
+export const isPublicKey = (pk: unknown): pk is PublicKey => {
+  if (pk instanceof PublicKey) {
+    return true;
+  }
+
+  if (typeof pk !== "object" || Array.isArray(pk)) {
+    return false;
+  }
+
+  try {
+    new PublicKey(pk as PublicKeyData);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
