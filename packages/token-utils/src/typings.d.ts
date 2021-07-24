7182e9b3da170c8ead705c4dce0ad5ece4200c75
Ian Macalinao <ian@saber.so>
2021-07-23 23:02:00 -0400

    Remove declare from typings

--- packages/token-utils/src/typings.d.ts
+++ packages/token-utils/src/typings.d.ts
@@ -1,15 +1,15 @@
 declare module "buffer-layout" {
-  declare const blob: (amt: number, property: string) => Layout;
-  declare const u8: (property: string) => Layout;
-  declare const ns64: (property: string) => Layout;
+  const blob: (amt: number, property: string) => Layout;
+  const u8: (property: string) => Layout;
+  const ns64: (property: string) => Layout;
 
-  declare type Layout<T = unknown> = {
+  type Layout<T = unknown> = {
     span: number;
     decode: (data: Buffer) => T;
     encode: (data: T, out: Buffer) => number;
   };
 
-  declare function struct<T>(
+  function struct<T>(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     layouts: Layout<any>[],
     property?: string
