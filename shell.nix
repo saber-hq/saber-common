40f45a63a4ae3736a538b7fc992589bcfd441f73
Ian Macalinao <ian@saber.so>
2021-07-30 19:58:47 -0400

    Update flakes (#124)

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 { pkgs }:
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    nodejs-14_x
-    (yarn.override { nodejs = nodejs-14_x; })
-  ];
+  buildInputs = with pkgs;
+    [ nodejs-16_x (yarn.override { nodejs = nodejs-16_x; }) ]
+    ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin
+      (with pkgs.darwin.apple_sdk.frameworks; [ CoreServices ]));
   shellHook = ''
     export PATH=node_modules/.bin:$PATH
   '';
