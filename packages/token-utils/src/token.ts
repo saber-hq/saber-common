55a0508943eb13606f84aad01ed4b6e358da6c93
Ian Macalinao <ian@saber.so>
2021-08-04 17:44:25 -0500

    Add helpers for chainId and constructing tokens quickly

--- packages/token-utils/src/token.ts
+++ packages/token-utils/src/token.ts
@@ -1,7 +1,6 @@
 import type { Network } from "@saberhq/solana-contrib";
 import { NATIVE_MINT } from "@solana/spl-token";
 import type { TokenInfo } from "@solana/spl-token-registry";
-import { ENV } from "@solana/spl-token-registry";
 import { PublicKey } from "@solana/web3.js";
 import type { Token as UToken } from "@ubeswap/token-math";
 
@@ -14,14 +13,7 @@ export class Token implements UToken<Token> {
 
   constructor(public readonly info: TokenInfo) {
     this.mintAccount = new PublicKey(info.address);
-    this.network =
-      info.chainId === ENV.MainnetBeta
-        ? "mainnet-beta"
-        : info.chainId === ENV.Devnet
-        ? "devnet"
-        : info.chainId === ENV.Testnet
-        ? "testnet"
-        : "localnet";
+    this.network = chainIdToNetwork(info.chainId) ?? "localnet";
   }
 
   public get chainId(): number {
@@ -57,6 +49,30 @@ export class Token implements UToken<Token> {
       this.decimals
     }, network=${this.network}]`;
   }
+
+  /**
+   * Loads a token from a Mint.
+   * @param mint
+   * @param opts
+   * @returns
+   */
+  static fromMint = (
+    mint: PublicKey | string,
+    decimals: number,
+    opts: Partial<Omit<TokenInfo, "address" | "decimals">> = {}
+  ): Token =>
+    new Token({
+      ...opts,
+
+      // required
+      address: mint.toString(),
+      decimals,
+
+      // optional
+      name: opts.name ?? `Token ${mint.toString().slice(0, 4)}`,
+      symbol: opts.symbol ?? "TOK",
+      chainId: opts.chainId ?? ChainId.Localnet,
+    });
 }
 
 export const tokensEqual = (
@@ -88,13 +104,50 @@ const sol = {
 export const makeTokenForAllNetworks = (
   token: Omit<TokenInfo, "chainId">
 ): TokenMap => ({
-  "mainnet-beta": new Token({ ...token, chainId: ENV.MainnetBeta }),
-  devnet: new Token({ ...token, chainId: ENV.Devnet }),
-  testnet: new Token({ ...token, chainId: ENV.Testnet }),
-  localnet: new Token({ ...token, chainId: 99999 }),
+  "mainnet-beta": new Token({ ...token, chainId: ChainId.MainnetBeta }),
+  devnet: new Token({ ...token, chainId: ChainId.Devnet }),
+  testnet: new Token({ ...token, chainId: ChainId.Testnet }),
+  localnet: new Token({ ...token, chainId: ChainId.Localnet }),
 });
 
 /**
  * Solana native token.
  */
 export const SOL: TokenMap = makeTokenForAllNetworks(sol);
+
+// comes from @solana/spl-token-registry, except we've added localnet
+export enum ChainId {
+  MainnetBeta = 101,
+  Testnet = 102,
+  Devnet = 103,
+  Localnet = 104,
+}
+
+export const NETWORK_TO_CHAIN_ID = {
+  "mainnet-beta": ChainId.MainnetBeta,
+  devnet: ChainId.Devnet,
+  testnet: ChainId.Testnet,
+  localnet: 104,
+};
+
+export const CHAIN_ID_TO_NETWORK: { [E in ChainId]: Network } = Object.entries(
+  NETWORK_TO_CHAIN_ID
+).reduce((acc, [network, env]) => ({ ...acc, [env]: network }), {}) as {
+  [E in ChainId]: Network;
+};
+
+/**
+ * Gets the chain id associated with a network.
+ * @param network
+ * @returns
+ */
+export const networkToChainId = (network: Network): ChainId =>
+  NETWORK_TO_CHAIN_ID[network];
+
+/**
+ * Gets the Network associated with a chain id.
+ * @param network
+ * @returns
+ */
+export const chainIdToNetwork = (env: ChainId): Network =>
+  CHAIN_ID_TO_NETWORK[env];
