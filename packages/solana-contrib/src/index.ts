58667b7cf449625805c0c4b71a56cc5e3a9b9a03
Ian Macalinao <ian@saber.so>
2021-08-05 14:57:49 -0500

    Adds SignerWallet (#152)
    * Add Keypair wallet to solana-contrib
    
    * Rename to SignerWallet
    
    * docstring
    
    * Move secret key adapter to use SignerWallet

--- packages/solana-contrib/src/index.ts
+++ packages/solana-contrib/src/index.ts
@@ -3,3 +3,4 @@ export * from "./interfaces";
 export * from "./provider";
 export * from "./transaction";
 export * from "./utils";
+export * from "./wallet";
---
+++ packages/solana-contrib/src/wallet.ts
@@ -0,0 +1,43 @@
+import type {
+  Connection,
+  PublicKey,
+  Signer,
+  Transaction,
+} from "@solana/web3.js";
+
+import type { Provider, Wallet } from "./interfaces";
+import { SolanaProvider } from "./provider";
+
+/**
+ * Wallet based on a Signer.
+ */
+export class SignerWallet implements Wallet {
+  constructor(public readonly signer: Signer) {}
+
+  get publicKey(): PublicKey {
+    return this.signer.publicKey;
+  }
+
+  signAllTransactions(transactions: Transaction[]): Promise<Transaction[]> {
+    return Promise.resolve(
+      transactions.map((tx) => {
+        tx.partialSign(this.signer);
+        return tx;
+      })
+    );
+  }
+
+  signTransaction(transaction: Transaction): Promise<Transaction> {
+    transaction.partialSign(this.signer);
+    return Promise.resolve(transaction);
+  }
+
+  /**
+   * Creates a Provider from this Wallet by adding a Connection.
+   * @param connection
+   * @returns
+   */
+  createProvider(connection: Connection): Provider {
+    return new SolanaProvider(connection, this);
+  }
+}
