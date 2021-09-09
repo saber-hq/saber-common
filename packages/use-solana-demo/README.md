170da4adb55d4cb5ba3c58f9544df04a89161850
Ian Macalinao <ian@saber.so>
2021-09-09 00:34:45 -0500

    Upgrade all dependencies

--- packages/use-solana-demo/README.md
+++
@@ -1,7 +0,0 @@
-# use-solana-demo
-
-This is an example app which uses `use-solana`.
-
-## Developing
-
-Run `parcel start`.
--- packages/use-solana-demo/components/Demo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { useConnectWallet } from "./WalletConnectorProvider";
-
-export const Demo: React.FC = () => {
-  const connect = useConnectWallet();
-  return <button onClick={connect}>Connect wallet</button>;
-};
