6d9a35f739a3b9544e759785185abb6a38f2acea
Ian Macalinao <ian@saber.so>
2021-10-30 11:59:40 -0700

    Make quote return TokenAmount

--- packages/token-utils/src/price.ts
+++ packages/token-utils/src/price.ts
@@ -1,5 +1,23 @@
 import { Price as UPrice } from "@ubeswap/token-math";
 
 import type { Token } from "./token";
+import { TokenAmount } from "./tokenAmount";
 
-export class Price extends UPrice<Token> {}
+/**
+ * A price of one token relative to another.
+ */
+export class Price extends UPrice<Token> {
+  override quote(tokenAmount: TokenAmount): TokenAmount {
+    const amt = super.quote(tokenAmount);
+    return new TokenAmount(this.quoteCurrency, amt.raw);
+  }
+
+  static fromUPrice(price: UPrice<Token>): Price {
+    return new Price(
+      price.baseCurrency,
+      price.quoteCurrency,
+      price.denominator,
+      price.numerator
+    );
+  }
+}
