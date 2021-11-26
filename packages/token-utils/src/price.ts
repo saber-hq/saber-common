b51905760177b421683b9e10634b5fa55199ee2a
Ian Macalinao <ian@saber.so>
2021-11-26 14:43:25 -0800

    price constructor

--- packages/token-utils/src/price.ts
+++ packages/token-utils/src/price.ts
@@ -1,3 +1,4 @@
+import type { BigintIsh } from "@ubeswap/token-math";
 import { Price as UPrice } from "@ubeswap/token-math";
 import invariant from "tiny-invariant";
 
@@ -8,6 +9,22 @@ import { TokenAmount } from "./tokenAmount";
  * A price of one token relative to another.
  */
 export class Price extends UPrice<Token> {
+  /**
+   * Constructs a price.
+   * @param baseCurrency
+   * @param quoteCurrency
+   * @param denominator
+   * @param numerator
+   */
+  public constructor(
+    baseCurrency: Token,
+    quoteCurrency: Token,
+    denominator: BigintIsh,
+    numerator: BigintIsh
+  ) {
+    super(baseCurrency, quoteCurrency, denominator, numerator);
+  }
+
   public override invert(): Price {
     return new Price(
       this.quoteCurrency,
