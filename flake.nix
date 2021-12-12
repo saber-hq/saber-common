125ee2c1c0f5522762e2bc99ee83c5f5b73567cd
Ian Macalinao <ian@saber.so>
2021-12-12 14:43:53 -0800

    nix shell

--- flake.nix
+++ flake.nix
@@ -13,12 +13,11 @@
       "x86_64-darwin"
     ] (system:
       let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ nodejs-16_x (yarn.override { nodejs = nodejs-16_x; }) ]
-            ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin
-              (with pkgs.darwin.apple_sdk.frameworks; [ CoreServices ]));
+      in rec {
+        packages.ci = pkgs.buildEnv {
+          name = "ci";
+          paths = with pkgs; [ nodejs yarn ];
         };
+        devShell = pkgs.mkShell { buildInputs = [ packages.ci ]; };
       });
 }
