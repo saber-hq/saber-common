85fb3534926108468c240f6a42e30551082e9c3b
Ian Macalinao <ian@saber.so>
2021-12-12 15:28:01 -0800

    Improve types for chai-solana

---
+++ packages/chai-solana/src/types.ts
@@ -0,0 +1,40 @@
+/// <reference types="chai" />
+
+import "chai-bn";
+import "chai-as-promised";
+
+import type { Address } from "@project-serum/anchor";
+import type { BigintIsh } from "@saberhq/token-utils";
+import type { TokenAmount } from "@solana/web3.js";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Chai {
+    export interface TokenAmountComparer {
+      (value: TokenAmount | BigintIsh, message?: string): void;
+    }
+    export interface TokenAmountAssertion {
+      equal: TokenAmountComparer;
+      equals: TokenAmountComparer;
+      eq: TokenAmountComparer;
+      // above: TokenAmountComparer;
+      // greaterThan: TokenAmountComparer;
+      // gt: TokenAmountComparer;
+      // gte: TokenAmountComparer;
+      // below: TokenAmountComparer;
+      // lessThan: TokenAmountComparer;
+      // lt: TokenAmountComparer;
+      // lte: TokenAmountComparer;
+      // least: TokenAmountComparer;
+      // most: TokenAmountComparer;
+      // closeTo: BNCloseTo;
+      // negative: BNBoolean;
+      zero: () => void;
+    }
+    interface Assertion {
+      eqAddress: (otherKey: Address, message?: string) => Assertion;
+      eqAmount: (otherAmount: TokenAmount, message?: string) => Assertion;
+      tokenAmount: TokenAmountAssertion;
+    }
+  }
+}
