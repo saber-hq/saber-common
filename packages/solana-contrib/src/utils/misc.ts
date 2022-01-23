7394f057af29e99868d2e4be83c42ea9c9f7e637
Ian Macalinao <ian@saber.so>
2022-01-23 13:53:13 -0800

    feat: adds falsy value filterers

--- packages/solana-contrib/src/utils/misc.ts
+++ packages/solana-contrib/src/utils/misc.ts
@@ -37,3 +37,39 @@ export const suppressConsoleError = <T>(fn: () => T): T => {
     throw e;
   }
 };
+
+/**
+ * Checks to see if the provided value is not null.
+ *
+ * Useful for preserving types in filtering out non-null values.
+ *
+ * @param value
+ * @returns
+ */
+export const isNotNull = <TValue>(value: TValue | null): value is TValue => {
+  return value !== null;
+};
+
+/**
+ * Checks to see if the provided value is not undefined.
+ *
+ * @param value
+ * @returns
+ */
+export const isNotUndefined = <TValue>(
+  value: TValue | undefined
+): value is TValue => {
+  return value !== undefined;
+};
+
+/**
+ * Checks to see if the provided value is not null or undefined.
+ *
+ * @param value
+ * @returns
+ */
+export const exists = <TValue>(
+  value: TValue | null | undefined
+): value is TValue => {
+  return value !== null && value !== undefined;
+};
