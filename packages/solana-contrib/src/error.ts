9e087812ca345cb7828d45e9199bbfffce121f2e
michaelhly <michaelhly@gmail.com>
2022-03-18 19:36:57 -0500

    Clean up error logging

--- packages/solana-contrib/src/error.ts
+++ packages/solana-contrib/src/error.ts
@@ -1,6 +1,8 @@
 export const firstAggregateError = (err: AggregateError) => {
   const errors = err.errors as Error[];
   const [firstError, ...remErrors] = [errors.pop(), ...errors];
-  console.error(remErrors);
+  if (remErrors.length > 0) {
+    console.error(remErrors);
+  }
   return firstError;
 };
