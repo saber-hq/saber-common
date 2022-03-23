0d8c7c3a4ae669a5c1590151635491924acccf20
Michael Huang <michaelhly@gmail.com>
2022-03-22 19:34:17 -0500

    Compute budget program instructions (#528)
    * Add compute budget program helpers
    
    * Add compute budget instruction helpers
    
    * fix yarn size

---
+++ packages/solana-contrib/src/computeBudget/instructions.ts
@@ -0,0 +1,38 @@
+import { TransactionInstruction } from "@solana/web3.js";
+
+import { COMPUTE_BUDGET_PROGRAM } from "./index";
+import { RequestHeapFrameLayout, RequestUnitsLayout } from "./layouts";
+
+/**
+ * Request a specific maximum number of compute units the transaction is
+ * allowed to consume and an additional fee to pay.
+ */
+export const requestComputeUnitsInstruction = (
+  units: number,
+  additionalFee: number
+): TransactionInstruction => {
+  const data = Buffer.alloc(RequestUnitsLayout.span);
+  RequestUnitsLayout.encode({ instruction: 0, units, additionalFee }, data);
+  return new TransactionInstruction({
+    data,
+    keys: [],
+    programId: COMPUTE_BUDGET_PROGRAM,
+  });
+};
+
+/**
+ * Request a specific transaction-wide program heap region size in bytes.
+ * The value requested must be a multiple of 1024. This new heap region
+ * size applies to each program executed, including all calls to CPIs.
+ */
+export const requestHeapFrameInstruction = (
+  bytes: number
+): TransactionInstruction => {
+  const data = Buffer.alloc(RequestHeapFrameLayout.span);
+  RequestHeapFrameLayout.encode({ instruction: 1, bytes }, data);
+  return new TransactionInstruction({
+    data,
+    keys: [],
+    programId: COMPUTE_BUDGET_PROGRAM,
+  });
+};
