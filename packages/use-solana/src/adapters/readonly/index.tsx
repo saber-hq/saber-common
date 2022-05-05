780626fbeae90105e5adffc606a98e52c3f20842
Ian Macalinao <ian@saber.so>
2022-05-05 10:57:23 -0500

    feat: allow Solana pubkey readonly overrides

--- packages/use-solana/src/adapters/readonly/index.tsx
+++ packages/use-solana/src/adapters/readonly/index.tsx
@@ -4,14 +4,32 @@ import { EventEmitter } from "eventemitter3";
 
 import type { WalletAdapter } from "../types";
 
+declare global {
+  interface Window {
+    /**
+     * Allows setting the pubkey for the ReadonlyAdapter.
+     */
+    USE_SOLANA_PUBKEY_OVERRIDE?: string;
+  }
+}
+
+/**
+ * Sets the readonly Solana pubkey.
+ * @param pubkey
+ */
+export const setReadonlySolanaPubkey = (pubkey: PublicKey): void => {
+  window.USE_SOLANA_PUBKEY_OVERRIDE = pubkey.toString();
+};
+
 export class ReadonlyAdapter extends EventEmitter implements WalletAdapter {
   private _publicKey: PublicKey | null = null;
 
   constructor() {
     super();
-
     const localPubkey =
-      process.env.REACT_APP_LOCAL_PUBKEY ?? process.env.LOCAL_PUBKEY;
+      window.USE_SOLANA_PUBKEY_OVERRIDE ??
+      process.env.REACT_APP_LOCAL_PUBKEY ??
+      process.env.LOCAL_PUBKEY;
     if (!localPubkey) {
       console.warn("LOCAL_PUBKEY not set for readonly provider");
     } else {
