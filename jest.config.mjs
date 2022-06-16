e7440e55ab90380ce17741fc2e176b61938035c3
Ian Macalinao <ian@saber.so>
2022-06-16 12:06:00 -0500

    switch to jest preset

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,15 +2,10 @@
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
+  preset: "ts-jest/presets/default-esm",
   globals: {
     "ts-jest": {
       useESM: true,
-      tsconfig: {
-        types: ["node", "jest"],
-      },
     },
   },
   moduleNameMapper: {
